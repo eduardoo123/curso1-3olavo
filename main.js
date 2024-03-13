3da945006962567813e20d6e75e1bcf517e4d62d
eduardoo123 <106556313+eduardoo123@users.noreply.github.com>
2024-03-13 11:36:10 -0300

    save

--- main.js
+++ main.js
@@ -16,10 +16,20 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-10-01T00:00:00"); 
 
-contadores[0].textContext = "Contagem Regressiva";
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+
+
+for (let i=0; i<contadores.length;i++) {
+    
+contadores[0].textContent = calculaTempo(tempos [0]);
+
+}
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
