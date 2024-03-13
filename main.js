fa5de4abbe65515ec52732d89d582f2b0bc679ab
eduardoo123 <106556313+eduardoo123@users.noreply.github.com>
2024-03-13 11:54:22 -0300

    salvo

--- main.js
+++ main.js
@@ -24,11 +24,6 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 
 
-for (let i=0; i<contadores.length;i++) {
-    
-contadores[0].textContent = calculaTempo(tempos [0]);
-
-}
 
 
 function calculaTempo(tempoObjetivo) {
@@ -38,14 +33,30 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+    
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    if (tempoFinal > 0) {
+        
+     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {}
+
 }
 
+function atualizaCronometro(){
+for (let i=0; i<contadores.length;i++) {
+contadores[0].textContent = calculaTempo(tempos [i]);
+    }
+}
 
+function comecaCronometro() {
+atualizaCronometro ();
+setInterval(atualizaCronometro, 1000);
+
+}
+
+comecaCronometro();
 
 
 
