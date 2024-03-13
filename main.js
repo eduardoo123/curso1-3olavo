023b43789a94e4be4b217b69a2f0e116b246adc8
eduardoo123 <106556313+eduardoo123@users.noreply.github.com>
2024-03-13 11:01:46 -0300

    salvo

--- main.js
+++ main.js
@@ -14,27 +14,28 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-    const contadores = document.querySelectorAll("contador");
-    const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-    let tempoAtual = new Date ();
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
 
-    contadores [0].textContext = "Contagem Regressiva";
-    contadores [0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContext = "Contagem Regressiva";
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
-    function calculaTempo(tempoObjetivo){
-        let tempoAtual = new Date() ;
-        let tempoFinal = tempoObjetivo - tempoAtual;
-        let segundos = Math.floor (tempoFinal / 1000);
-        let minutos = Math.floor (segundos / 60);
-        let horas = Math.floor (minutos / 60);
-        let dias = Math.floor (horas / 24);
-        
-        segundos %= 60;
-        minutos %= 60;
-        horas %= 24;
-        return dias;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-    }
-        
-    
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
+
+
+
+
 
